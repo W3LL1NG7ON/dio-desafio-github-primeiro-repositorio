03fc0ebe92d3d7767439f83b4785e8e6db3b5199
well <wellingtonfl1986@gmail.com>
2022-02-04 08:42:16 -0300

    atualização do arquivo js

--- Projeto jogo resgate/js/js.js
+++ Projeto jogo resgate/js/js.js
@@ -10,6 +10,26 @@ function start() { // Inicio da função start()
     //Principais variáveis do jogo
 	
 	var jogo = {}
+	var velocidade=5;
+	var posicaoY = parseInt(Math.random() * 334);
+	var TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+		}
+	
+	jogo.pressionou = [];
+	//Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+		jogo.pressionou[e.which] = true;
+		});
+	
+	
+		$(document).keyup(function(e){
+		   jogo.pressionou[e.which] = false;
+		});
+	
 	
 	//Game Loop
 
@@ -18,6 +38,15 @@ function start() { // Inicio da função start()
 	function loop() {
 	
 	movefundo();
+
+	movejogador();
+
+	moveinimigo1();
+
+	moveinimigo2();
+
+	moveamigo();
+	
 	
 	} // Fim da função loop()
 
@@ -31,6 +60,74 @@ function start() { // Inicio da função start()
 	$("#fundoGame").css("background-position",esquerda-1);
 	
 	} // fim da função movefundo()
+
+	function movejogador() {
+	
+		if (jogo.pressionou[TECLA.W]) {
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo-10);
+			if (topo<=20) {
+		
+				$("#jogador").css("top",topo+10);
+			}
+		
+		}
+		
+		if (jogo.pressionou[TECLA.S]) {
+			
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo+10);
+			if (topo>=400) {	
+				$("#jogador").css("top",topo-10);
+					
+			}	
+		}
+		
+		if (jogo.pressionou[TECLA.D]) {
+			
+			//Chama função Disparo	
+		}
+	
+		} // fim da função movejogador()
+
+		function moveinimigo1() {
+
+			posicaoX = parseInt($("#inimigo1").css("left"));
+			$("#inimigo1").css("left",posicaoX-velocidade);
+			$("#inimigo1").css("top",posicaoY);
+				
+				if (posicaoX<=0) {
+				posicaoY = parseInt(Math.random() * 334);
+				$("#inimigo1").css("left",694);
+				$("#inimigo1").css("top",posicaoY);
+					
+				}
+		} //Fim da função moveinimigo1()
+
+		function moveinimigo2() {
+			posicaoX = parseInt($("#inimigo2").css("left"));
+		$("#inimigo2").css("left",posicaoX-3);
+					
+			if (posicaoX<=0) {
+				
+			$("#inimigo2").css("left",775);
+						
+			}
+	} // Fim da função moveinimigo2()
+
+	function moveamigo() {
+	
+		posicaoX = parseInt($("#amigo").css("left"));
+		$("#amigo").css("left",posicaoX+1);
+					
+			if (posicaoX>906) {
+				
+			$("#amigo").css("left",0);
+						
+			}
+	
+	} // fim da função moveamigo()
+	
 
 
 } // Fim da função start
